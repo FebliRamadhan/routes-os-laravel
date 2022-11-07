26a28a1afb01fbc0fa584f2a2b97d16d4420e754
FebliRamadhan <ramadhan.febli11@gmail.com>
2022-11-07 10:40:44 +0700

    initial commit

---
+++ app/Http/Controllers/RoutersOsController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Validator;
+use Illuminate\Http\Request;
+
+use \RouterOS\Client;
+use \RouterOS\Query;
+
+class RoutersOsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'mac' => 'string|required'
+        ]);
+
+        if($validator->fails())
+        {
+            return false;
+        }
+
+        $client = new Client([
+            'host' => env('MIKROTIK_IP'),
+            'user' => env('MIKROTIK_USERNAME'),
+            'pass' => env('MIKROTIK_PASSWORD'),
+            'port' => 8728,
+        ]);
+
+        $query = (new Query('/ip/hotspot/host/print'))
+                ->where('mac-address', strtoupper($request->mac));
+
+        $response = $client->query($query)->read();
+        
+        if(count($response) > 0) {
+            $remove = (new Query('/ip/hotspot/host/remove'))
+            ->equal('.id', $response[0]['.id']);
+            $client->query($remove)->read();
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
